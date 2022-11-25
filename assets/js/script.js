510b3d841387f9210442b95af8de7addf08db6b0
dachatorn <dachatorn.a@kegroup.co.th>
2022-11-25 13:43:49 +0700

    texture

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,6 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-/**
  * Sizes
  */
 const sizes = {
@@ -49,7 +41,7 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
 camera.position.y = 1
-camera.position.z = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
